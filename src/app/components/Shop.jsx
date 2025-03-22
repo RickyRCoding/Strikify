f994670782a851e310b5e806ce7a165134569c63
Ricardo Rizk <ricardorizk2@gmail.com>
2025-03-22 09:55:53 -0400

    Enhance Shop component with feedback section and style updates

--- src/app/components/Shop.jsx
+++ src/app/components/Shop.jsx
@@ -4,7 +4,7 @@ import Product from "./Product.jsx";
 export default function Shop({ onAddItemToCart }) {
   return (
     <section id="shop">
-      <h2>
+      <h2 className="section-title">
         High quality, 3D printed artisan keycaps for any one that wants a splash
         to their computer setup.
       </h2>
@@ -15,6 +15,16 @@ export default function Shop({ onAddItemToCart }) {
           </li>
         ))}
       </ul>
+      <hr />
+      <h2 className="form-title">
+        Did you like your experience shopping online? Please give us your
+        feedback:
+      </h2>
+      <textarea
+        className="form"
+        placeholder="Write your feedback here"
+      ></textarea>
+      <button className="form-button">Submit</button>
     </section>
   );
 }
